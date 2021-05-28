871c485344620fa3706023401cfd374a0065804f
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-05-28 16:49:03 +0200

    Update announcement.js

--- commands/announcement.js
+++ commands/announcement.js
@@ -3,12 +3,12 @@ const discord = require("discord.js");
 module.exports.run = async(bot,message,args) =>{
 
     // !announcement title | bericht | kleur | kanaal
-    var allowedRole = message.guild.roles.find("name","Seraphim");
+    var allowedRole = message.guild.RoleManager.highest
    
-    if(message.member.roles.has(allowedRole.id)){
-        return message.reply(`deze gebruiker is: ${roles}`);
-    }
-   
+    //if(message.member.roles.has(allowedRole.id)){
+      //  return message.reply(`deze gebruiker is: ${roles}`);
+    //}
+   console.log(allowedRole);
 }
 
 
