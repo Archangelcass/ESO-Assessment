e4e3f89c3c39631897430aa43c32167523b4bd8d
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-05-29 09:18:55 +0200

    Update spreadsheet.js

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -9,8 +9,8 @@ var creds = require('../client_secret.json');
   creds.private_key_id = process.env.private_key_id.replace(/\\n/g, '\n');
   creds.private_key = process.env.private_key.replace(/\\n/g, '\n');
   creds.client_id = process.env.client_id.replace(/\\n/g, '\n');
-  creds.client_x509_cert_url - process.env.client_x509_cert_url.replace(/\\n/g, '\n');
-  creds.project_id - process.env.project_id.replace(/\\n/g, '\n');
+  creds.client_x509_cert_url = process.env.client_x509_cert_url.replace(/\\n/g, '\n');
+  creds.project_id = process.env.project_id.replace(/\\n/g, '\n');
 
 async function accessSpreadsheet(){
   
