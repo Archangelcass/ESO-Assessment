593648e9dfdb3cd48a122e1e96d05f7260d0c5b5
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-05-29 13:07:03 +0200

    Update spreadsheet.js

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -46,7 +46,7 @@ async function accessSpreadsheet() {
   var naam = row.Naam; 
   var level = row.LVL;
   var dcID = row.DiscordID;
-  var user = guild.members.fetch(row.DiscordID).promise;
+  var user = guild.members.fetch(row.DiscordID);
  // var roles = user._roles;
   console.log(user);
 
