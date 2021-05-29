9956dec15c3f857a434ee73002e004ad2a53fdb8
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-05-29 09:13:41 +0200

    Update spreadsheet.js

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -5,10 +5,6 @@ const { promisify } = require('util');
 
 var creds = require('../client_secret.json');
 
-
-
-async function accessSpreadsheet(){
-  
   creds.client_email = process.env.client_email.replace(/\\n/g, '\n');
   creds.private_key_id = process.env.private_key_id.replace(/\\n/g, '\n');
   creds.private_key = process.env.private_key.replace(/\\n/g, '\n');
@@ -16,19 +12,25 @@ async function accessSpreadsheet(){
   creds.client_x509_cert_url - process.env.client_x509_cert_url.replace(/\\n/g, '\n');
   creds.project_id - process.env.project_id.replace(/\\n/g, '\n');
 
+async function accessSpreadsheet(){
   
-  const doc = new GoogleSpreadsheet('1CbuRYQIrwCGrVBWQ_MxONIuaQkHkb2MHp6vLu04alAY'); 
+const doc = new GoogleSpreadsheet('1CbuRYQIrwCGrVBWQ_MxONIuaQkHkb2MHp6vLu04alAY'); 
+await promisify(doc.useServiceAccountAuth)(creds);
+const info = await promisify(doc.getInfo)();
+const sheet = info.worksheetsp[0];
+console.log(`Title: ${sheet.title} . Rows: ${sheet.rowcCount}`);
     
 }
+
 
 
 //const creds = require('./client_secret.json');
 
   module.exports.run = async(bot,message,args) =>{
 
-  /*accessSpreadsheet();
+  accessSpreadsheet();
 
-    console.log(process.env.client_email);
+ /*   console.log(process.env.client_email);
     console.log(process.env.private_key);
     
     await doc.useServiceAccountAuth({
