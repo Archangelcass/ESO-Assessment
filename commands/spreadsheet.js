29623599fcc08420978b069a6583392dabfd5f45
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-06-02 15:49:58 +0200

    Update spreadsheet.js

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -24,7 +24,7 @@ async function accessSpreadsheet() {
 }
 
   module.exports.run = async(bot,message,args) =>{
-  if(args =="Show"){  
+  if(args[1] =="Show"){  
     var guild = message.guild;
     var guildicon = guild.iconURL();
     var usericon = message.author.avatarURL();
