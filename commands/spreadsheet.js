9948f20b659dc6db76caddab77fa86dfe560d5cf
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-06-04 15:49:21 +0200

    Update spreadsheet.js

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -80,7 +80,7 @@ else if(args[1] =="add"){
   console.log(doc.title);
 
   
-  console.log(message.mentions.members.first().roles);
+  console.log(message.mentions.members.first().roles.first);
 
   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
 
