785eeec324bb7be4665d0e178a5505b2de04188c
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-06-04 16:53:39 +0200

    Update spreadsheet.js

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -79,8 +79,8 @@ else if(args[1] =="add"){
   await doc.loadInfo(); // loads document properties and worksheets
   console.log(doc.title);
 
-  
-  console.log(message.mentions.members.first()._roles['0']);
+  role = message.mentions.members.first()._roles['0'];
+  console.log(`${role}`);
 
   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
 
