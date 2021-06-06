6bfb86b2ed48a8deeb711472fea6f0127d463f55
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-06-06 11:41:45 +0200

    Update namens.js

--- commands/namens.js
+++ commands/namens.js
@@ -7,36 +7,12 @@ module.exports.run = async(bot,message,args) =>{
         console.log('Ik kom hier uberhaupt terecht');
         client.channels.get(args[1]).send("test bericht");
 
-
-    let filter = (msg) => !msg.author.bot;
-
-    let options = {
-
-        max:2,
-        time:60
-    };
-
-    let collector = msg.channel.createMessageCollector(filter,options)
-
-    collector.on('collect', (m) => {
-        console.log(`Collected ${m.content}`);
-    });
-
-    collector.on('end', (collected) => {
-        console.log(`Collected ${collected.size} items`);
-    });
-
-    msg.reply('Welk bericht wil je mee sturen?');
-
-   
-    }
-
    
     return message.channel.send("Hallo!");
 
 
 }
-
+}
 module.exports.help = {
     name: "hallo"
 }
