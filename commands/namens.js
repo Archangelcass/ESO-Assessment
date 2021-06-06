a8329fbc5cb72b1e4b0f199d6f4bc1bd2fdd389e
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-06-06 11:40:12 +0200

    Update namens.js

--- commands/namens.js
+++ commands/namens.js
@@ -4,6 +4,7 @@ module.exports.run = async(bot,message,args) =>{
 
 
     if(args[1]){ 
+        console.log('Ik kom hier uberhaupt terecht');
         client.channels.get(args[1]).send("test bericht");
 
 
