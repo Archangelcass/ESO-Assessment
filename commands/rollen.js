166ffd465a673ed96f289190d4809555298537dd
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-08-27 11:16:51 +0200

    Update rollen.js

--- commands/rollen.js
+++ commands/rollen.js
@@ -3,19 +3,8 @@ const discord = require("discord.js");
 module.exports.run = async(bot,message,args) =>{
 
     console.log(args);
-    var guild = message.guild;
-    var guildicon = guild.iconURL();
-    var usericon = message.author.avatarURL();
 
-
-    if(args[1]){ 
-        console.log('Ik kom hier uberhaupt terecht');
-        
-
-
-        }
-
-
+    
     return message.channel.send("Message Received");
 
 
