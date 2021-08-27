ca5d736e98344ff63ae48a07940bb78c23ba7c12
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-08-27 11:22:23 +0200

    Update rollen.js

--- commands/rollen.js
+++ commands/rollen.js
@@ -4,10 +4,19 @@ module.exports.run = async(bot,message,args) =>{
 
     console.log(args);
 
+    if (args[1]){
+    var Mention = args[1];
+    var mention = (`<@&${Mention}>`);
+    let member = message.mentions.members.first();
+
+    console.log(member);
+    }
+
     
+
     return message.channel.send("Message Received");
 
-
+    
 
 }
 module.exports.help = {
