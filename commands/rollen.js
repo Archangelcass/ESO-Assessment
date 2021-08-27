178cf4ef944d385f9cb9b887d56efa2764334de4
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-08-27 11:34:06 +0200

    Update rollen.js

--- commands/rollen.js
+++ commands/rollen.js
@@ -4,19 +4,23 @@ module.exports.run = async(bot,message,args) =>{
 
     console.log(args);
 
-    if (args[1]){
+    if (args[1] == "Verified"){
+        if (args[2]){
     var Mention = args[1];
-    var mention = (`<@&${Mention}>`);
     let member = message.mentions.members.first();
-
-    let roleID = "866995362212347914"
     
-    member.roles.add(roleID).catch(console.error);
+    let AssignedRole1 = "829664202527473695"
+    let AssignedRole2 = "832177644685885440"
+    let AssignedRole3 = "852439559567310879"
+    
+    member.roles.add(AssignedRole1).catch(console.error);
+    member.roles.add(AssignedRole2).catch(console.error);
+    member.roles.add(AssignedRole3).catch(console.error);
 
     console.log(member);
 
-    message.channel.send("Gebruiker:"+''+mention+' '+"Heeft nu de role(s):"+`<@&${roleID}>`+"Extra")
-    }
+    message.channel.send("Gebruiker:"+''+Mention+' '+"Heeft nu de role(s):"+`<@&${AssignedRole1}>,<@&${AssignedRole2}>,<@&${AssignedRole3}>`+"Extra")
+    }}
 
     
 
