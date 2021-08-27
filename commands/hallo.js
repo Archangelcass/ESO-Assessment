c502ccacbc22366ba819b23a7c4fda9277293649
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-08-27 12:10:58 +0200

    Update hallo.js

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,7 +2,12 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot,message,args) =>{
 
-    return message.channel.send("Hallo!");
+    message.channel.send("Hallo, hoe kan ik helpen?!");
+
+    const filter = message => message.author.id === guildmember.id
+    channel.awaitMessages(filter, { max: 4, time: 60, errors: 'time'})
+    .then(collected => console.log(collected.size))
+    .catch(collected => console.log())
 
 
 }
