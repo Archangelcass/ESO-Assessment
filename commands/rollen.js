294697ec7235f6ee955cac997134384289a854dd
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-08-27 11:44:02 +0200

    Update rollen.js

--- commands/rollen.js
+++ commands/rollen.js
@@ -9,17 +9,20 @@ module.exports.run = async(bot,message,args) =>{
     var Mention = args[2];
     let member = message.mentions.members.first();
     
-    let AssignedRole1 = "829664202527473695"
-    let AssignedRole2 = "832177644685885440"
-    let AssignedRole3 = "852439559567310879"
+    let AssignedRole1 = "829664202527473695";
+    let AssignedRole2 = "832177644685885440";
+    let AssignedRole3 = "852439559567310879";
+    let RemovedRole1 = "837235279953657887";
     
     member.roles.add(AssignedRole1).catch(console.error);
     member.roles.add(AssignedRole2).catch(console.error);
     member.roles.add(AssignedRole3).catch(console.error);
+    member.roles.remove(RemovedRole1).catch(console.error);
 
     console.log(member);
 
     message.channel.send("Gebruiker:"+''+Mention+' '+"Heeft nu de role(s):"+`<@&${AssignedRole1}>,<@&${AssignedRole2}>,<@&${AssignedRole3}>`+"Extra")
+    message.channel.send("Gebruiker:"+''+Mention+' '+"Heeft nu niet meer de role(s):"+`<@&${RemovedRole1}>`);
     }}
 
     
