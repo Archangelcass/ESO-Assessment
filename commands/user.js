aed241bac9d4a4916403b52a08c4f2d435b62504
Archangelcass <84969309+Archangelcass@users.noreply.github.com>
2021-08-28 15:27:16 +0200

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -24,7 +24,7 @@ module.exports.run = async(bot,message,args) =>{
         .then( message => {
             message = message.first();
             console.log(message.content); 
-            let member = message.mentions.members.first();
+            var member = message.mentions.members.first();
          console.log(mentionedmember);
          
          member.roles.add(AssignedRole1).catch(console.error);
